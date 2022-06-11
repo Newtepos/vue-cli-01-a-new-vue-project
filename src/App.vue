48c969048d4883398c2fcb8579734c024b241a3f
Supakit <supakit.pnm@gmail.com>
2022-06-11 12:17:36 +0700

    create: get start with Vue CLI

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <section>
+    <h2>My frineds</h2>
+    <ul>
+      <friend-contact/>
+      <friend-contact/>
+    </ul>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      frineds: [
+        {
+          id: "supakit",
+          name: "supakit pavaputanon",
+          phone: "063 020 0081",
+          email: "supakit.pnm@gmail.com",
+        },
+        {
+          id: "cheese",
+          name: "thipsukol kobprasertsri",
+          phone: "081 753 2754",
+          email: "thipsukol@gmail.com",
+        },
+      ],
+    };
+  },
+};
+</script>
